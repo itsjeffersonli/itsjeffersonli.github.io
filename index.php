658a0fe0aac79e3a67be934a9d335899ae2f8bac
run3 <53053198+itsjeffersonli@users.noreply.github.com>
2020-10-30 00:53:41 +0800

    123

---
+++ index.php
@@ -0,0 +1,213 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="John Jefferson Li" />
+    <meta name="author" content="John Jefferson Li" />
+    <meta property="og:url" content="johnjeffersonli.rf.gd" />
+    <meta property="og:title" content="John Jefferson Li" />
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content="John Jefferson Li">
+    <title>John Jefferson Li</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/profile.png" />
+    <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+
+<body id="page-top">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-danger fixed-top" id="sideNav">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">
+            <span class="d-block d-lg-none">John Jefferson Li</span>
+            <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="assets/img/profile.png" alt="" /></span>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Awards</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container-fluid p-0">
+        <section class="resume-section" id="about">
+            <div class="resume-section-content">
+                <h1 class="mb-0">
+                    John Jefferson
+                    <span class="text-danger">Li</span>
+                </h1>
+                <div class="subheading mb-5">
+                    · Manila, Philippines · (+63)917-869-3896 ·
+                </div>
+                <p class="lead mb-5">Junior Penetration Tester, Exploit Developer, CTF Player, Bug Bounty Hunter, Full Stack Web Developer</p>
+                <div class="social-icons">
+                    <a class="social-icon" href="https://www.linkedin.com/in/john-jefferson-li-3b86811b0/"><i class="fab fa-linkedin-in"></i></a>
+                    <a class="social-icon" href="https://github.com/itsjeffersonli"><i class="fab fa-github"></i></a>
+                    <a class="social-icon" href="mailto:lijohnjefferson@gmail.com"><i class="fas fa-envelope"></i></a>
+                    <a class="social-icon" href="#"><i class="fab fa-twitter"></i></a>
+                    <a class="social-icon" href="#"><i class="fab fa-facebook-f"></i></a>
+                </div>
+                <br/>
+                <a style="margin-left: 10px; margin-top: 10px" href="https://www.hackthebox.eu/profile/223654"><img src="http://www.hackthebox.eu/badge/image/223654" alt="Hack The Box"></a>
+                <a style="margin-left: 10px; margin-top: 10px" href="https://codetrace.com/users/itsjeffersonli"><img src="https://codetrace.com/widget/itsjeffersonli" width="220" height="50" /></a>
+                <br>
+                <br>
+                <codersrank-widget username="itsjeffersonli" badges="3" layout="auto" style="--bg-color: #292b2c; --header-bg-color: #d9534f; --badge-bg-color: #c4dce4; --badge-text-color: #111; width: 455px; margin-left: 10px;"></codersrank-widget>
+            </div>
+        </section>
+        <hr class="m-0" />
+        <section class="resume-section" id="experience">
+            <div class="resume-section-content">
+                <h2 class="mb-5 text-danger">Experience</h2>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Cyber Security Researcher</h3>
+                        <div class="subheading mb-3">Bug Crowd</div>
+                        <div style="margin-left: 20px;">
+                            <p>— Conduct Penetration Testing</p>
+                            <p>— Web Application Security Assessment</p>
+                            <p>— Report Found Vulnerabilities</p>
+                        </div>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-danger">April 2020 - Present</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Cyber Security Researcher</h3>
+                        <div class="subheading mb-3">Secuna</div>
+                        <div style="margin-left: 20px;">
+                            <p>— Conduct Penetration Testing</p>
+                            <p>— Web Application Security Assessment</p>
+                            <p>— Report Found Vulnerabilities</p>
+                        </div>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-danger">June 2020 - Present</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Cyber Security Researcher</h3>
+                        <div class="subheading mb-3">Hackerone</div>
+                        <div style="margin-left: 20px;">
+                            <p>— Conduct Penetration Testing</p>
+                            <p>— Web Application Security Assessment</p>
+                            <p>— Report Found Vulnerabilities</p>
+                        </div>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-danger">July 2020 - Present</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">CTF Player</h3>
+                        <div class="subheading mb-3">Team Rootless</div>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-danger">April 2020 - July 2020</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">CTF Player</h3>
+                        <div class="subheading mb-3">Team PWNHUB</div>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-danger">August 2020 - Present</span></div>
+                </div>
+            </div>
+        </section>
+        <hr class="m-0" />
+        <section class="resume-section" id="education">
+            <div class="resume-section-content">
+                <h2 class="mb-5 text-danger">Education</h2>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">De La Salle College of Saint Benilde</h3>
+                        <div class="subheading mb-3">Bachelor of Science</div>
+                        <div>Information Systems - Information Security</div>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-danger">Currently Enrolled</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Philippine Institute of Quezon City</h3>
+                        <div class="subheading mb-3">High School Diploma</div>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-danger">March 2019</span></div>
+                </div>
+            </div>
+        </section>
+        <hr class="m-0" />
+        <section class="resume-section" id="skills">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Skills</h2>
+                <div class="subheading mb-3">Languages</div>
+                <ul class="list-inline dev-icons">
+                    <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/html-5.png" /></li>
+                    <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/css3.png" /></li>
+                    <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/bootstrap.png" /></li>
+                    <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/php.png" /></li>
+                    <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/vue-js.png" /></li>
+                    <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/python.png" /></li>
+                    <li class="list-inline-item"><img src="https://img.icons8.com/office/48/000000/console.png" /></li>
+                    <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/c-sharp-logo.png" /></li>
+                </ul>
+                <div class="subheading mb-3">Tools</div>
+                <ul class="list-inline dev-icons">
+                    <li class="list-inline-item"><img src="https://img.icons8.com/fluent/48/000000/github.png" /></li>
+                    <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/visual-studio.png" /></li>
+                    <li class="list-inline-item"><img src="https://img.icons8.com/fluent/48/000000/visual-studio-code-2019.png" /></li>
+                    <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/git.png" /></li>
+                    <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/virtualbox.png" /></li>
+                </ul>
+                <div class="subheading mb-3">Operating Systems</div>
+                <ul class="list-inline dev-icons">
+                    <li class="list-inline-item"><img src="https://img.icons8.com/fluent/48/000000/windows-10.png" /></li>
+                    <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/linux.png" /></li>
+                    <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/ubuntu.png" /></li>
+                    <li class="list-inline-item"><img src="https://img.icons8.com/material/48/000000/mac-client.png" /></li>
+                    <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/kali-linux.png" /></li>
+                    <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/parrot.png" /></li>
+                </ul>
+            </div>
+        </section>
+        <hr class="m-0" />
+        <section class="resume-section" id="awards">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Awards & Certifications</h2>
+                <ul class="fa-ul mb-0">
+                    <li>
+                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span> <a href="/assets/img/Penetration Testing.png" class="text-success"> Penetration Testing with Ethical Hacking</a>
+                        <br>
+                        <span style="margin-left: 10px;">Nexus IT Training Center · November 2019</span>
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span> <a href="/assets/img/CEH.png" class="text-success"> Certified Ethical Hacker</a>
+                        <br>
+                        <span style="margin-left: 10px;">Study 365 · December 2019</span>
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span> <a href="/assets/img/CEH.png" class="text-success"> Native to FullStack Development</a>
+                        <br>
+                        <span style="margin-left: 10px;">CodeStack PH · Febuary 2020</span>
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span> <a href="https://www.credential.net/ce2f619d-c6a5-4b60-989a-42c395d00ef4" class="text-success"> Certified Network Security Specialist</a>
+                        <br>
+                        <span style="margin-left: 10px;">ICSI · May 2020</span>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <script src="https://profile.codersrank.io/widget/widget.js"></script>
+    <script src="/js/scripts.js"></script>
+</body>
+
+</html>
